7ae37d29ed83c95a0bc17f95a25816d366e04b9c
Ciaran Donnachie <ciaran.donnachie@gmail.com>
2019-07-18 17:45:48 +0100

    reminder application created. Ability to reminder message and title

---
+++ imports/ui/body.js
@@ -0,0 +1,32 @@
+import { Template } from 'meteor/templating';
+import { Reminders } from '../api/reminders.js';
+import './body.html';
+
+Template.body.helpers({
+  reminders() {
+    return Reminders.find({}, { sort: { createdAt: -1 } });
+  }
+});
+
+Template.body.events({
+  'submit .new-reminder'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+    const title = target.title.value;
+
+    // Insert reminder into collection
+    Reminders.insert({
+      title: title,
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+    target.title.value = '';
+  }
+});

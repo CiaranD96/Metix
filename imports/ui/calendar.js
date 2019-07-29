ee82af34130957811a45e782c93a9708d2fa65f7
Ciaran Donnachie <ciaran.donnachie@gmail.com>
2019-07-29 21:13:12 +0100

    users can add a reminder to fullcalendar by clicking or clicking and dragging mouse over calendar dates

--- imports/ui/calendar.js
+++ imports/ui/calendar.js
@@ -7,10 +7,44 @@ import './calendar.html';
 Template.calendar.onRendered(() => {
   $('#calendarDiv').fullCalendar({
     header: {
-      left: 'basicDay, basicWeek, month',
+      left: 'prev, next today',
       center: 'title',
-      right: 'today prev,next'
+      right: 'month, agendaWeek, agendaDay'
     },
-    editable: true
+    // Calendar Settings
+    editable: true,
+    selectable: true,
+    selectHelper: false,
+    eventLimit: true,
+
+    select: function(start, end) {
+      // User can add a reminder by clicking / clicking and dragging over days on the calendar
+      var title = prompt('Reminder Title');
+      var desc = prompt('Reminder Description');
+      var eventData;
+
+      if (title && desc && time) {
+        eventData = {
+          title: title,
+          description: desc,
+          start: start,
+          end: end
+        };
+        $('#calendarDiv').fullCalendar('renderEvent', eventData, true);
+      }
+      $('#calendarDiv').fullCalendar('unselect');
+    },
+    // Display event title, description and time in a window
+    eventClick: function(event) {
+      alert(
+        'Event title: ' +
+          event.title +
+          '\nEvent description: ' +
+          event.description
+      );
+    },
+    eventMouseover: function(calEvent) {
+      $(this).css('background-color', 'lightblue');
+    }
   });
 });
